ec1aab65db2bc02ea648df37d13300efd26d3856
ErnstRoell <ernstroell@gmail.com>
2024-06-19 17:05:07 +0200

    Initial commit with extra documentation.

---
+++ dect/directions.py
@@ -0,0 +1,96 @@
+"""
+Helper function to generate a structured set of directions in 2 or 3 dimensions.
+"""
+
+import torch
+
+
+def generate_uniform_directions(num_thetas: int = 64, d: int = 3, device: str = "cpu"):
+    """Generate randomly sampled directions from a sphere in d dimensions"""
+    v = torch.randn(size=(d, num_thetas), device=device)
+    v /= v.pow(2).sum(axis=0).sqrt().unsqueeze(1)
+    return v
+
+
+def generate_uniform_2d_directions(num_thetas: int = 64, device: str = "cpu"):
+    """Generate uniformly sampled directions on the unit sphere."""
+    v = torch.vstack(
+        [
+            torch.sin(torch.linspace(0, 2 * torch.pi, num_thetas, device=device)),
+            torch.cos(torch.linspace(0, 2 * torch.pi, num_thetas, device=device)),
+        ]
+    )
+
+    return v
+
+
+# Needs some work.
+def generate_structured_directions(
+    num_thetas: int = 64, d: int = 3, device: str = "cpu"
+):
+    """
+    Generates a structured set of directions in either 2 or 3 dimensions.
+    If the dimension is two, we evenly space the directions along the unit
+    circle.
+    In the 3d case we sample along three unit circles, the xy, xz and yz planes.
+    Along these unit circles we sample evenly. A check is performed to ensure
+    the number of directions provided is divisible by three.
+    """
+
+    if d == 3:
+        if num_thetas % 3 != 0:
+            raise ValueError(
+                f"Number of theta's should be divisible by three, you provided {num_thetas}"
+            )
+        w1 = torch.vstack(
+            [
+                torch.sin(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+                torch.cos(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+                torch.zeros_like(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+            ]
+        )
+
+        w2 = torch.vstack(
+            [
+                torch.sin(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+                torch.zeros_like(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+                torch.cos(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+            ]
+        )
+
+        w3 = torch.vstack(
+            [
+                torch.zeros_like(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+                torch.sin(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+                torch.cos(
+                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
+                ),
+            ]
+        )
+        v = torch.hstack([w1, w2, w3])
+    elif d == 2:
+        v = torch.vstack(
+            [
+                torch.sin(torch.linspace(0, 2 * torch.pi, num_thetas, device=device)),
+                torch.cos(torch.linspace(0, 2 * torch.pi, num_thetas, device=device)),
+            ]
+        )
+    else:
+        raise ValueError("d should be either 2 or three")
+    return v
---
+++ dect/ect.py
@@ -0,0 +1,236 @@
+"""
+Implementation of the ECT.
+"""
+
+from dataclasses import dataclass
+import torch
+from torch import nn
+from torch_scatter import segment_add_coo
+
+
+@dataclass(frozen=True)
+class EctConfig:
+    """
+    Configuration of the ECT Layer.
+    """
+
+    num_thetas: int = 32
+    bump_steps: int = 32
+    radius: float = 1.1
+    ect_type: str = "points"
+    device: str = "cpu"
+    num_features: int = 3
+    normalized: bool = False
+
+
+@dataclass()
+class Batch:
+    """Template of the required attributes for a data batch.
+
+    Parameters
+    ----------
+    x : torch.FloatTensor
+        The coordinates of the nodes in the simplical complex provided in the
+        format [num_nodes,feature_size].
+    batch: torch.LongTensor
+        An index that indicates to which pointcloud a point belongs to, in
+        principle automatically created by torch_geometric when initializing the
+        batch.
+    edge_index: torch.LongTensor
+        The indices of the points that span an edge in the graph. Conforms to
+        pytorch_geometric standards. Shape has to be of the form [2,num_edges].
+    face:
+        The indices of the points that span a face in the simplicial complex.
+        Conforms to pytorch_geometric standards. Shape has to be of the form
+        [3,num_faces].
+    """
+
+    x: torch.FloatTensor
+    batch: torch.LongTensor
+    edge_index: torch.LongTensor | None = None
+    face: torch.LongTensor | None = None
+
+
+def compute_ecc(
+    nh: torch.FloatTensor,
+    index: torch.LongTensor,
+    lin: torch.FloatTensor,
+    scale: float = 100,
+) -> torch.FloatTensor:
+    """Computes the Euler Characteristic curve.
+
+    Parameters
+    ----------
+    nh : torch.FloatTensor
+        The node heights, computed as the inner product of the node coordinates
+        x and the direction vector v.
+    index: torch.LongTensor
+        The index that indicates to which pointcloud a node height belongs. For
+        the node heights it is the same as the batch index, for the higher order
+        simplices it will have to be recomputed.
+    lin: torch.FloatTensor
+        The discretization of the interval [-1,1] each node height falls in this
+        range due to rescaling in normalizing the data.
+    out: torch.FloatTensor
+        The shape of the resulting tensor after summation. It has to be of the
+        shape [num_discretization_steps, batch_size, num_thetas]
+    scale: torch.FloatTensor
+        A single number that scales the sigmoid function by multiplying the sigmoid
+        with the scale. With high (100>) values, the ect will resemble a discrete
+        ECT and with lower values it will smooth the ECT.
+    """
+    ecc = torch.nn.functional.sigmoid(scale * torch.sub(lin, nh))
+    return segment_add_coo(ecc.movedim(0, 1), index)
+
+
+def compute_ecc_derivative(
+    nh: torch.FloatTensor,
+    index: torch.LongTensor,
+    lin: torch.FloatTensor,
+    scale: float = 100,
+):
+    """
+    Computes the Euler Characteristic curve with the derivative of the sigmoid.
+    """
+    ecc = torch.nn.functional.sigmoid(scale * torch.sub(lin, nh)) * (
+        1 - torch.nn.functional.sigmoid(scale * torch.sub(lin, nh))
+    )
+    return segment_add_coo(ecc.movedim(0, 1), index)
+
+
+def compute_ect_points_derivative(
+    data: Batch, v: torch.FloatTensor, lin: torch.FloatTensor
+):
+    """
+    Computes the ECT of a pointcloud with the derivative of the sigmoid.
+    """
+    nh = data.x @ v
+    return compute_ecc_derivative(nh, data.batch, lin)
+
+
+def compute_ect_points(batch: Batch, v: torch.FloatTensor, lin: torch.FloatTensor):
+    """Computes the Euler Characteristic Transform of a batch of point clouds.
+
+    Parameters
+    ----------
+    batch : Batch
+        A batch of data containing the node coordinates and batch index.
+    v: torch.FloatTensor
+        The direction vector that contains the directions.
+    lin: torch.FloatTensor
+        The discretization of the interval [-1,1] each node height falls in this
+        range due to rescaling in normalizing the data.
+    out: torch.FloatTensor
+        The shape of the resulting tensor after summation. It has to be of the
+        shape [num_discretization_steps, batch_size, num_thetas]
+    """
+    nh = batch.x @ v
+    return compute_ecc(nh, batch.batch, lin)
+
+
+def compute_ect_edges(data: Batch, v: torch.FloatTensor, lin: torch.FloatTensor):
+    """Computes the Euler Characteristic Transform of a batch of graphs.
+
+    Parameters
+    ----------
+    batch : Batch
+        A batch of data containing the node coordinates, the edges and batch index.
+    v: torch.FloatTensor
+        The direction vector that contains the directions.
+    lin: torch.FloatTensor
+        The discretization of the interval [-1,1] each node height falls in this
+        range due to rescaling in normalizing the data.
+    out: torch.FloatTensor
+        The shape of the resulting tensor after summation. It has to be of the
+        shape [num_discretization_steps, batch_size, num_thetas]
+    """
+    # Compute the node heigths
+    nh = data.x @ v
+
+    # Perform a lookup with the edge indices on node heights, this replaces the
+    # node index with its node height and then compute the maximum over the
+    # columns to compute the edge height.
+    eh, _ = nh[data.edge_index].max(dim=0)
+
+    # Compute which batch an edge belongs to. We take the first index of the
+    # edge (or faces) and do a lookup on the batch index of that node in the
+    # batch indices of the nodes.
+    batch_index_nodes = data.batch
+    batch_index_edges = data.batch[data.edge_index[0]]
+
+    return compute_ecc(nh, batch_index_nodes, lin) - compute_ecc(
+        eh, batch_index_edges, lin
+    )
+
+
+def compute_ect_faces(data: Batch, v: torch.FloatTensor, lin: torch.FloatTensor):
+    """Computes the Euler Characteristic Transform of a batch of meshes.
+
+    Parameters
+    ----------
+    batch : Batch
+        A batch of data containing the node coordinates, edges, faces and batch
+        index.
+    v: torch.FloatTensor
+        The direction vector that contains the directions.
+    lin: torch.FloatTensor
+        The discretization of the interval [-1,1] each node height falls in this
+        range due to rescaling in normalizing the data.
+    out: torch.FloatTensor
+        The shape of the resulting tensor after summation. It has to be of the
+        shape [num_discretization_steps, batch_size, num_thetas]
+    """
+    # Compute the node heigths
+    nh = data.x @ v
+
+    # Perform a lookup with the edge indices on node heights, this replaces the
+    # node index with its node height and then compute the maximum over the
+    # columns to compute the edge height.
+    eh, _ = nh[data.edge_index].max(dim=0)
+
+    # Do the same thing for the faces.
+    fh, _ = nh[data.face].max(dim=0)
+
+    # Compute which batch an edge belongs to. We take the first index of the
+    # edge (or faces) and do a lookup on the batch index of that node in the
+    # batch indices of the nodes.
+    batch_index_nodes = data.batch
+    batch_index_edges = data.batch[data.edge_index[0]]
+    batch_index_faces = data.batch[data.face[0]]
+
+    return (
+        compute_ecc(nh, batch_index_nodes, lin)
+        - compute_ecc(eh, batch_index_edges, lin)
+        + compute_ecc(fh, batch_index_faces, lin)
+    )
+
+
+class EctLayer(nn.Module):
+    """Machine learning layer for computing the ECT."""
+
+    def __init__(self, config: EctConfig, V=None):
+        super().__init__()
+        self.config = config
+        self.lin = (
+            torch.linspace(-config.radius, config.radius, config.bump_steps)
+            .view(-1, 1, 1)
+            .to(config.device)
+        )
+
+        self.v = V
+
+        if config.ect_type == "points":
+            self.compute_ect = compute_ect_points
+        elif config.ect_type == "edges":
+            self.compute_ect = compute_ect_edges
+        elif config.ect_type == "faces":
+            self.compute_ect = compute_ect_faces
+        elif config.ect_type == "points_derivative":
+            self.compute_ect = compute_ect_points_derivative
+
+    def forward(self, batch: Batch):
+        """Forward method for the ECT Layer."""
+        ect = self.compute_ect(batch, self.v, self.lin)
+        if self.config.normalized:
+            return ect / torch.amax(ect, dim=(1, 2)).unsqueeze(1).unsqueeze(1)
+        return ect
