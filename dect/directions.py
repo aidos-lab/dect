17529bef0a5d8eee469a7d883072caf34a3ecb86
Ernst Roell <ernstroell@gmail.com>
2024-06-20 16:35:30 +0200

    Added multiview directions sampling

--- dect/directions.py
+++ dect/directions.py
@@ -3,7 +3,7 @@ Helper function to generate a structured set of directions in 2 and 3 dimensions
 """
 
 import torch
-
+import itertools
 
 def generate_uniform_directions(num_thetas: int = 64, d: int = 3, device: str = "cpu"):
     """
@@ -55,74 +55,32 @@ def generate_uniform_2d_directions(num_thetas: int = 64, device: str = "cpu"):
     return v
 
 
-# Needs some work.
-def generate_structured_directions(
-    num_thetas: int = 64, d: int = 3, device: str = "cpu"
-):
+def generate_multiview_directions(num_thetas: int, bump_steps: int, d: int):
     """
-    Generates a structured set of directions in either 2 or 3 dimensions.
-    If the dimension is two, we evenly space the directions along the unit
-    circle.
-    In the 3d case we sample along three unit circles, the xy, xz and yz planes.
-    Along these unit circles we sample evenly. A check is performed to ensure
-    the number of directions provided is divisible by three.
+    Generates multiple sets of structured directions in n dimensions.
+
+    We generate sets of directions by embedding the 2d unit circle in 
+    d dimensions and sample this unit circle in a structured fashion. 
+    This generates d choose 2 structured directions that are organized 
+    in channels, compatible with the ECT calculations. 
+
+    After computing the ECT, we obtain an d choose 2 channel image where 
+    each channel consists of a structured ect along a hyperplane. 
+    For the 3-d case we would obtain a 3 channel ect with direction sampled 
+    along the xy, xz and yz planes in three dimensions.  
     """
-    assert d in (2, 3), "You should provide either 2 or 3 for the dimension."
+    w = torch.vstack([
+        torch.sin(torch.linspace(0, 2*torch.pi, bump_steps)),
+        torch.cos(torch.linspace(0, 2*torch.pi, bump_steps)),
+    ])
 
-    if d == 3:
-        if num_thetas % 3 != 0:
-            raise ValueError(
-                f"Number of theta's should be divisible by three, you provided {num_thetas}"
-            )
-        w1 = torch.vstack(
-            [
-                torch.sin(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-                torch.cos(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-                torch.zeros_like(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-            ]
-        )
+    # We obtain n choose 2 channels. 
+    idx_pairs = list(itertools.combinations(range(d), r=2))
 
-        w2 = torch.vstack(
-            [
-                torch.sin(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-                torch.zeros_like(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-                torch.cos(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-            ]
-        )
+    v = torch.zeros(size=(len(idx_pairs), d, num_thetas))
 
-        w3 = torch.vstack(
-            [
-                torch.zeros_like(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-                torch.sin(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-                torch.cos(
-                    torch.linspace(0, 2 * torch.pi, num_thetas // 3, device=device)
-                ),
-            ]
-        )
-        v = torch.hstack([w1, w2, w3])
-    elif d == 2:
-        v = torch.vstack(
-            [
-                torch.sin(torch.linspace(0, 2 * torch.pi, num_thetas, device=device)),
-                torch.cos(torch.linspace(0, 2 * torch.pi, num_thetas, device=device)),
-            ]
-        )
-    else:
-        raise ValueError("d should be either 2 or three")
+    for idx, idx_pair in enumerate(idx_pairs):
+        v[idx, idx_pair[0], :] = w[0]
+        v[idx, idx_pair[1], :] = w[1]
+
     return v
