6a619a0e4a3085b8553712a7e434739faf485452
Ernst Röell <ernstroell@gmail.com>
2025-01-20 12:19:13 +0100

    Added support for both sigmoid, indicator and arbitary functions.

---
+++ dect/ect_fn.py
@@ -0,0 +1,20 @@
+"""Elementwise operations for the calculation of the ECC"""
+
+from typing import TypeAlias
+import torch
+
+Tensor: TypeAlias = torch.Tensor
+
+
+def indicator(ecc: Tensor) -> Tensor:
+    """
+    Indicator function for the calculation of the ect.
+
+    Args:
+        ecc (Tensor):
+        The height values.
+
+    Returns:
+        Tensor: The ECC.
+    """
+    return torch.heaviside(ecc, torch.tensor([0.0]))
